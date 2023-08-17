5830683366ccf31b5d6cfabab1172b2e69de1814
John Doe <israel.padilha@gmail.com>
2023-08-17 15:51:58 -0300

    Exercise 5: incremental procedure

---
+++ lista6/5/Ex007.pas
@@ -0,0 +1,16 @@
+program incrementa_uma_unidade;
+var n: longint;
+
+procedure incrementa(var num: longint);
+begin
+	num := num + 1;
+end;
+
+begin
+    n:= 1;
+    while (n <= 10) do
+    begin
+        writeln (n);
+        incrementa (n);
+    end;
+end.
